f1b4ae08bc630d4d8c562cbdfb58e7edb05ac645
Ken Rimple <krimple@chariotsolutions.com>
2015-05-25 09:12:04 -0400

    added mouse move tests with link and bind example

--- src/dragger.js
+++ src/dragger.js
@@ -3,29 +3,28 @@
     angular.module('myApp')
     .directive('dragger', function() {
         return {
-            scope: true,
+            scope: false,
             restrict: 'E',
             template: '<div ng-mousemove="moveMe($event)"' +
                       ' ng-mousedown="downMe($event)" ' +
                       ' ng-mouseup="upMe($event)"> ' +
                       ' </div>',
-            controller: function($log, $scope) {
-                $scope.downMe = function($event) {
-                    $scope.lastEvent = 'down';
-                    $scope.pageX = $event.pageX;
-                    $scope.pageY = $event.pageY;
-                };
-                $scope.upMe = function($event) {
-                    $scope.lastEvent = 'up';
-                    $scope.pageX = $event.pageX;
-                    $scope.pageY = $event.pageY;
-                };
-                $scope.moveMe = function($event) {
-                    console.log('you moved me');
-                    $scope.lastEvent = 'move';
-                    $scope.pageX = $event.pageX;
-                    $scope.pageY = $event.pageY;
-                };
+            link: function(scope, element, attributes) {
+                element.bind('mousemove', function($event) {
+                    scope.lastEvent = 'move';
+                    scope.pageX = $event.pageX;
+                    scope.pageY = $event.pageY;
+                });
+                element.bind('mouseup', function($event) {
+                    scope.lastEvent = 'up';
+                    scope.pageX = $event.pageX;
+                    scope.pageY = $event.pageY;
+                });
+                element.bind('mousedown', function($event) {
+                    scope.lastEvent = 'down';
+                    scope.pageX = $event.pageX;
+                    scope.pageY = $event.pageY;
+                });                
             }
         };
     });
--- test/dragger-spec.js
+++ test/dragger-spec.js
@@ -1,15 +1,22 @@
 (function() {
     'use strict';
     describe('da dragger!', function() {
-        var element;
+        var element, $timeout, $compile, $scope;
 
         beforeEach(module('myApp'));
-        it('should down the mouse!', inject(function($timeout, $compile, $rootScope) {
-            var $scope = $rootScope.$new();
+        beforeEach(inject(function($rootScope, _$timeout_, _$compile_) {
+            $timeout = _$timeout_;
+            $compile = _$compile_;
+            $scope = $rootScope.$new();
             element = $compile(angular.element('<dragger id="foo"></dragger>'))($scope);
+        }));
+
+        it('should create the dom element', function() {
             expect(element).toBeDefined();
             console.log(angular.mock.dump(element));
+        });
 
+        it('should move the mouse!', function() {
             $timeout(function() {
                 element.triggerHandler({
                     pageX: 100,
@@ -24,7 +31,41 @@
             expect($scope.lastEvent).toBe('move');
             expect($scope.pageX).toBe(100);
             expect($scope.pageY).toBe(100);
-        }));
+        });
+
+        it('should down the mouse!', function() {
+            $timeout(function() {
+                element.triggerHandler({
+                    pageX: 100,
+                    pageY: 100,
+                    type: 'mousedown'
+                });
+            }, 100);
+            $timeout.flush();
+
+            // now it executes the ng-click bound function and we're good
+            console.log(angular.mock.dump($scope));
+            expect($scope.lastEvent).toBe('down');
+            expect($scope.pageX).toBe(100);
+            expect($scope.pageY).toBe(100);
+        });
+
+        it('should up the mouse!', function() {
+            $timeout(function() {
+                element.triggerHandler({
+                    pageX: 100,
+                    pageY: 100,
+                    type: 'mouseup'
+                });
+            }, 100);
+            $timeout.flush();
+
+            // now it executes the ng-click bound function and we're good
+            console.log(angular.mock.dump($scope));
+            expect($scope.lastEvent).toBe('up');
+            expect($scope.pageX).toBe(100);
+            expect($scope.pageY).toBe(100);
+        });
     });
 }());
 
